311135f864481fa6a24d5c4243419ed056533902
yaltha <yaltharullist@gmail.com>
2021-05-09 00:11:51 +0700

    date-picker

--- screens/Rent/Rent2.tsx
+++ screens/Rent/Rent2.tsx
@@ -1,45 +1,58 @@
-import React, { useCallback, useState } from "react";
-import { View } from "react-native";
-import { Button } from "react-native-paper";
-import { DatePickerModal } from "react-native-paper-dates";
+import React, { FC } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-import { styles } from "./styles";
+import ModalDatePicker from 'react-native-datepicker-modal'
 
-const Rent = () => {
-    const [date, setDate] = useState<Date | undefined>(undefined);
-    const [open, setOpen] = useState(false);
+import colors from '../../config/colors'
+import spacing from '../../config/spacing'
+import fontSize from '../../config/fontSizes'
 
-    const onDismissSingle = useCallback(() => {
-        setOpen(false);
-    }, [setOpen]);
+type RentProps = {
+    style: {}
+}
 
-    const onConfirmSingle = useCallback(
-        (params) => {
-            setOpen(false);
-            setDate(params.date);
-        },
-        [setOpen, setDate]
-    );
-
+const Rent2: React.FC<RentProps> = ({
+    style,
+    ...props
+}) => {
     return (
-        <View
-            style={{
-                flex: 1,
-                marginTop: 50,
-            }}
-        >
-            <Button onPress={() => setOpen(true)} uppercase={false} mode="outlined">
-                Pick Single Date
-      </Button>
-            <DatePickerModal
-                mode="single"
-                visible={open}
-                date={date}
-                onDismiss={onDismissSingle}
-                onConfirm={onConfirmSingle}
-            />
-        </View>
-    );
-};
 
-export default Rent;
+        <ModalDatePicker
+            style={[styles.container, style]}
+            renderDate={({ year, month, day, date }) => {
+                if (!date) {
+                    return <Text style={[styles.text, styles.placeholderText]}>Date of Birth</Text>
+                }
+                const dateStr = `${day}-${month}-${year}`
+                return <Text style={styles.text}>{dateStr}</Text>
+            }}
+            {...props}
+        />
+    )
+}
+
+export default Rent2
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.white,
+        borderBottomColor: colors.gray.veryLight,
+        borderBottomWidth: 1,
+        marginVertical: spacing[1],
+        marginHorizontal: spacing[0],
+        justifyContent: 'center',
+        borderRadius: 2,
+        height: 50
+    },
+    placeholderText: {
+        color: colors.gray.light
+    },
+    text: {
+        width: '100%',
+        paddingHorizontal: spacing[1],
+        paddingVertical: spacing[0],
+        // fontFamily: 'Montserrat',
+        fontSize: fontSize.medium,
+        color: colors.gray.dark
+    }
+})
